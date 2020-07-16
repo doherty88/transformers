f4ca57b32f22fcb9db042e537b739fceb6f6923e
doherty88 <liudeyang@gmail.com>
2020-07-16 12:00:02 +0800

    update SST 3

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -336,6 +336,48 @@ class Sst2Processor(DataProcessor):
             examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
         return examples
 
+    
+class Sst3Processor(DataProcessor):
+    """Processor for the SST-3 data set (GFN Sentiment version)."""
+
+    def get_example_from_tensor_dict(self, tensor_dict):
+        """See base class."""
+        return InputExample(
+            tensor_dict["idx"].numpy(),
+            tensor_dict["sentence"].numpy().decode("utf-8"),
+            None,
+            str(tensor_dict["label"].numpy()),
+        )
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1", "2"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training, dev and test sets."""
+        examples = []
+        text_index = 1 if set_type == "test" else 0
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, i)
+            text_a = line[text_index]
+            label = None if set_type == "test" else line[1]
+            examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
+
 
 class StsbProcessor(DataProcessor):
     """Processor for the STS-B data set (GLUE version)."""
@@ -558,6 +600,7 @@ glue_tasks_num_labels = {
     "mnli": 3,
     "mrpc": 2,
     "sst-2": 2,
+    "sst-3": 3,
     "sts-b": 1,
     "qqp": 2,
     "qnli": 2,
@@ -571,6 +614,7 @@ glue_processors = {
     "mnli-mm": MnliMismatchedProcessor,
     "mrpc": MrpcProcessor,
     "sst-2": Sst2Processor,
+    "sst-3": Sst3Processor,
     "sts-b": StsbProcessor,
     "qqp": QqpProcessor,
     "qnli": QnliProcessor,
@@ -584,6 +628,7 @@ glue_output_modes = {
     "mnli-mm": "classification",
     "mrpc": "classification",
     "sst-2": "classification",
+    "sst-3": "classification",
     "sts-b": "regression",
     "qqp": "classification",
     "qnli": "classification",
