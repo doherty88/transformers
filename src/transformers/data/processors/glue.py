fa8cee945d6322be071df88fde6b893c7a814ab4
doherty88 <liudeyang@gmail.com>
2020-07-17 15:33:18 +0800

    Add GFN Class classification processor

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -377,6 +377,47 @@ class Sst3Processor(DataProcessor):
             label = None if set_type == "test" else line[1]
             examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
         return examples
+    
+class GfnClassProcessor(DataProcessor):
+    """Processor for the GFN NLP Class data set (GFN Sentiment version)."""
+
+    def get_example_from_tensor_dict(self, tensor_dict):
+        """See base class."""
+        return InputExample(
+            tensor_dict["idx"].numpy(),
+            tensor_dict["sentence"].numpy().decode("utf-8"),
+            None,
+            str(tensor_dict["label"].numpy()),
+        )
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1", "2", "3"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training, dev and test sets."""
+        examples = []
+        text_index = 1 if set_type == "test" else 0
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, i)
+            text_a = line[text_index]
+            label = None if set_type == "test" else line[1]
+            examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
 
 
 class StsbProcessor(DataProcessor):
@@ -601,6 +642,7 @@ glue_tasks_num_labels = {
     "mrpc": 2,
     "sst-2": 2,
     "sst-3": 3,
+    "nlp-class": 4,
     "sts-b": 1,
     "qqp": 2,
     "qnli": 2,
@@ -615,6 +657,7 @@ glue_processors = {
     "mrpc": MrpcProcessor,
     "sst-2": Sst2Processor,
     "sst-3": Sst3Processor,
+    "nlp-class": GfnClassProcessor,
     "sts-b": StsbProcessor,
     "qqp": QqpProcessor,
     "qnli": QnliProcessor,
@@ -629,6 +672,7 @@ glue_output_modes = {
     "mrpc": "classification",
     "sst-2": "classification",
     "sst-3": "classification",
+    "nlp-class": "classification",
     "sts-b": "regression",
     "qqp": "classification",
     "qnli": "classification",
